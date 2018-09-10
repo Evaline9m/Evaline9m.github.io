bf6521bb499eb6d4b9cd409746c223433d339f7c
zddddd <1367894069@qq.com>
2018-09-10 13:51:55 +0800

    新增滑动效果

--- index.js
+++ index.js
@@ -209,10 +209,44 @@ var set = {
 				arr[i] = arr[i]*2;
 			}
 		}
+	},
+	
+	//获得角度
+	getAngle:function(angx, angy){
+		return Math.atan2(angy, angx) * 180 / Math.PI;
+	},
+	
+	//根据起点终点返回方向 1向上 2向下 3向左 4向右 0未滑动
+	getDirection:function(startx, starty, endx, endy){
+			var angx = endx - startx;
+			var angy = endy - starty;
+			var result = 0;
+		
+			//如果滑动距离太短
+			if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+				return result;
+			}
+		
+			var angle = set.getAngle(angx, angy);
+			if (angle >= -135 && angle <= -45) {
+				result = 1;
+			} else if (angle > 45 && angle < 135) {
+				result = 2;
+			} else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+				result = 3;
+			} else if (angle >= -45 && angle <= 45) {
+				result = 4;
+			}
+		
+			return result;
 	}
 	
 	
 }
+
+
+
+    
 
 
 var className = ["a1","a2","a3","a4","b1","b2","b3","b4","c1","c2","c3","c4","d1","d2","d3","d4"];
@@ -221,6 +255,7 @@ var arr2=["1","2","3","4"];
 var num = 1;
 var topNum = 2;
 var start = document.getElementsByClassName("start")[0];
+var startx, starty;
 
 (function (){
 	set.init()
@@ -243,6 +278,35 @@ var start = document.getElementsByClassName("start")[0];
 			break;
 		}
 	}
+	
+	
+	//手指接触屏幕
+	document.addEventListener("touchstart", function(e) {
+		startx = e.touches[0].pageX;
+		starty = e.touches[0].pageY;
+	}, false);
+	//手指离开屏幕
+	document.addEventListener("touchend", function(e) {
+		var endx, endy;
+		endx = e.changedTouches[0].pageX;
+		endy = e.changedTouches[0].pageY;
+		var direction = set.getDirection(startx, starty, endx, endy);
+		switch (direction) {
+			case 1:
+				set.move("top")
+				break;
+			case 2:
+				set.move("bottom")
+				break;
+			case 3:
+				set.move("left")
+				break;
+			case 4:
+				set.move("right")
+				break;
+			default:
+		}
+	}, false);
 	
 	start.onclick = function(){
 		set.init()
